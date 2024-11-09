9f7c3da997b7d185990c2cc947b1b1c54f01e2f1
Sufiyan33 <basu700736@gmail.com>
2024-11-09 15:49:57 +0530

    Creating from control

--- crud-operations/src/app/app.component.ts
+++ crud-operations/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'crud-operations';
+  
+  employeeForm?: FormGroup;
+  employeeObj: EmployeeModel = new EmployeeModel();
+
+  constructor(){
+    this.createForm()
+  }
+  //Now create control for all fields
+  createForm(){
+    this.employeeForm = new FormGroup({
+      empId: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      state: new FormControl(this.employeeObj.state),
+      emailId: new FormControl(this.employeeObj.emailId),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      address: new FormControl(this.employeeObj.address),
+      pinCode: new FormControl(this.employeeObj.pinCode)
+    })
+  }
+  // Now let's bind this form to html fileds.
 }
