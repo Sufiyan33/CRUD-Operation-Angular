44c94d0d7f2b750a2200899799f0203fde301a2b
Sufiyan33 <basu700736@gmail.com>
2024-11-09 16:29:16 +0530

    Creating and saving data into local storage

--- crud-operations/src/app/app.component.ts
+++ crud-operations/src/app/app.component.ts
@@ -14,9 +14,16 @@ export class AppComponent {
   
   employeeForm: FormGroup = new FormGroup({});
   employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel[]=[];
 
   constructor(){
-    this.createForm()
+    this.createForm();
+    debugger;
+    const oldData = localStorage.getItem("empData");
+    if(oldData != null){
+      const parseData = JSON.parse(oldData);
+      this.employeeList = parseData;
+    }
   }
   //Now create control for all fields
   createForm(){
@@ -32,4 +39,29 @@ export class AppComponent {
     })
   }
   // Now let's bind this form to html fileds.
+
+  //Now let's add CRUD function for saving and removing employee
+  /*
+  Since we want empId auto generated, for that we need to check if local storage length
+  if there are data then increase by 1.
+
+  if page reload then local storage should be check auto, and also need to push data into local storage
+   for that you need to add some code in constructor.
+  */
+  onSave(){
+    debugger;
+    const oldData = localStorage.getItem("empData");
+    if(oldData != null){
+      const parseData = JSON.parse(oldData);
+      this.employeeForm.controls['empId'].setValue(parseData.length + 1);
+      this.employeeList.unshift(this.employeeForm.value);
+    }else{
+      this.employeeList.unshift(this.employeeForm.value);
+    }
+    localStorage.setItem("empData", JSON.stringify(this.employeeList))
+  }
+
+  onReset(){
+
+  }
 }
