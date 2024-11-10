90a1573faff2e2060f06b03e2de5f68c1ffd43f8
Sufiyan33 <basu700736@gmail.com>
2024-11-10 15:08:54 +0530

    Created costant file for validation

---
+++ crud-operations/src/app/constant/Constant.ts
@@ -0,0 +1,8 @@
+export const Constants = {
+    VALIDATION_MESSAGE:{
+        NAME_REQUIRED: 'Name is required',
+        NAME_LENGTH: 'Name length should be between 2 & 10',
+        PIN_CODE: 'Pin code is required',
+        PIN_CODE_LENGTH: 'Pin code length should be 6'
+    }
+}
