15dc86695042003444ccc1222c4ea7f65cce198a
Sufiyan33 <basu700736@gmail.com>
2024-11-10 16:43:46 +0530

    Added list of validations in constant file

--- crud-operations/src/app/constant/Constant.ts
+++ crud-operations/src/app/constant/Constant.ts
@@ -1,8 +1,20 @@
 export const Constants = {
     VALIDATION_MESSAGE:{
-        NAME_REQUIRED: 'Name is required',
-        NAME_LENGTH: 'Name should be not more than 15 char',
-        PIN_CODE: 'Pin code is required',
-        PIN_CODE_LENGTH: 'Pin code length should be 6'
+        NAME_REQUIRED: 'Name is require.',
+        NAME_LENGTH: 'Name should be not more than 15 char.',
+        NAME_PATTERN: 'Name only should be Alphabet.',
+        EMAIL_REQUIRED: 'Email is require.',
+        EMAIL_VALID: 'Email should be valid.',
+        CONTACT_REQUIRED: 'Contact no. is require.',
+        CONTACT_VALID: 'Contact no should only in digit',
+        CONTACT_LENGTH: 'Contact length should be between 0-10',
+        CITY_REQUIRED: 'City is require.',
+        CITY_VALID: 'City should in char',
+        STATE_REQUIRED: 'State is require.',
+        STATE_VALID: 'State should only in char.',
+        PIN_CODE: 'Pin code is require.',
+        PIN_CODE_LENGTH: 'Pin code length should be 6 digit.',
+        PIN_CODE_PATTERN: 'Pin code only in digit.',
+        ADDRESS_REQUIRED: 'Address is require.'
     }
 }
