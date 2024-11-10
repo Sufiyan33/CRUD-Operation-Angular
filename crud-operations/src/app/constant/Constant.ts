357a255e6184ecbeb107317eff2a53bf9392ec2c
Sufiyan33 <basu700736@gmail.com>
2024-11-10 16:27:05 +0530

    Correcting contant value

--- crud-operations/src/app/constant/Constant.ts
+++ crud-operations/src/app/constant/Constant.ts
@@ -1,7 +1,7 @@
 export const Constants = {
     VALIDATION_MESSAGE:{
         NAME_REQUIRED: 'Name is required',
-        NAME_LENGTH: 'Name length should be between 2 & 10',
+        NAME_LENGTH: 'Name should be not more than 15 char',
         PIN_CODE: 'Pin code is required',
         PIN_CODE_LENGTH: 'Pin code length should be 6'
     }
